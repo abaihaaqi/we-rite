f7d7c72212eb505d4a23ce1d72e6b18e0c120ba6
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-09-28 09:58:06 +0700

    Initial project 👌

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Layout from "../layouts";
+import "@fontsource/merriweather";
+import "@fontsource/poppins";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
